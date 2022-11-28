9a22b682710b271945b804491b01ecb77a5bb990
HAERYN <syt06119@naver.com>
2022-11-28 23:03:30 +0900

    feat: #6 정보공유 화면 개설 및 재배치

---
+++ src/screens/main/InformationScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+function InformationScreen() {
+    return (
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <Text>정보공유 화면</Text>
+        </View>
+    );
+}
+export default InformationScreen;
--- src/screens/main/index.tsx
+++ src/screens/main/index.tsx
@@ -3,14 +3,16 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import HomeScreen from './HomeScreen';
 import {MainBottomTabParamList} from './MainBottomTabParams';
 import CommunityScreen from './CommunityScreen';
+import InformationScreen from './InformationScreen'
 
 const BottomTab = createBottomTabNavigator<MainBottomTabParamList>();
 
 function MainScreen() {
   return (
     <BottomTab.Navigator>
-      <BottomTab.Screen name="Home" component={HomeScreen} options={{headerShown: false,}}/>
       <BottomTab.Screen name="Community" component={CommunityScreen} options={{headerShown: false,}}/>
+      <BottomTab.Screen name="Home" component={HomeScreen} options={{headerShown: false,}}/>
+      <BottomTab.Screen name="Information" component={InformationScreen} options={{headerShown: false,}}/>
     </BottomTab.Navigator>
   );
 }
